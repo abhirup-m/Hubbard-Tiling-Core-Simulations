6f5239be1dee99f80efa8b0f715c3f32b6956714
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2026-06-01 23:43:57 +0530

    better commented and simplified PhaseDiagram.jl

--- PhaseDiagram.jl
+++ PhaseDiagram.jl
@@ -2,91 +2,65 @@ using LinearAlgebra, Plots, ProgressMeter
 include("Constants.jl")
 include("RgFlow.jl")
 
-function PoleFraction(
+# Given a set of parameters (J, W), simulates the RG
+# equations for those parameters, and counts the number
+# of points N_p on Fermi surface that satisfy |J_{k,k}| > 0
+function CountPoles(
         size_BZ::Int64,
         J_val::Float64,
         W_val::Float64;
     )
+
+    # obtain fixed point Kondo matrix for given (J, W)
     kondoJArray = momentumSpaceRG(size_BZ, J_val, W_val)
+
+    # identify points on Fermi surface. This is independent
+    # of (J, W) and simply looks at the zero energy surface
+    # of the non-interacting dispersion
     _, dispersion = getDensityOfStates(tightBindDisp, size_BZ)
     fermiPoints = unique(getIsoEngCont(dispersion, 0.0))
+
+    # if certain J_{k,k'} matrix elements have fallen below
+    # a threshold at the fixed point, set them to zero,
+    # the threshold RG_RELEVANCE_TOL is set in Constants.jl
     averageKondoScale = J_val / 2
     @assert averageKondoScale > RG_RELEVANCE_TOL
     kondoJArray .= ifelse.(abs.(kondoJArray) ./ averageKondoScale .> RG_RELEVANCE_TOL, kondoJArray, 0)
-    polesFraction = count(p -> any(>(0), abs.(kondoJArray[p, fermiPoints])), fermiPoints)
-    return polesFraction
+
+    # count the number of points {k} on Fermi surface that satisfy |J_kk| > 0.
+    # this is taken to be the criteria for the said point remaining gapless.
+    poles = count(p -> any(>(0), abs.(kondoJArray[p, fermiPoints])), fermiPoints)
+
+    return poles
 end
 
 
-function CriticalBathInt(
-        size_BZ::Int64,
-        omega_by_t::Float64,
-        kondoJ::Float64,
-        transitionWindow::Vector{Float64},
-        bathIntSpacing::Float64;
-        maxIter=100,
-        loadData::Bool=false,
-    )
-    fracToIndex(f) = ifelse(f == 1, 1, ifelse(f > 0, 2, 3))
-
-    savePathCrit = joinpath(SAVEDIR, "crit-$(size_BZ)-$(kondoJ).json")
-    criticalBathIntData = Dict{String,Vector{Float64}}()
-    if isfile(savePathCrit) && loadData
-        merge!(criticalBathIntData, JSON3.read(read(savePathCrit, String), typeof(criticalBathIntData)))
-        if string(bathIntSpacing) ∈ keys(criticalBathIntData)
-            return criticalBathIntData[string(bathIntSpacing)]
-        end
-    end
-    @assert bathIntSpacing > 0
-    criticalBathInt = Float64[]
-    @assert issorted(transitionWindow, rev=true)
-
-    savePath = joinpath(SAVEDIR, "pf-$(size_BZ)-$(kondoJ).json")
-    availableData = Dict{String,Float64}()
-    if isfile(savePath)
-        merge!(availableData, JSON3.read(read(savePath, String), typeof(availableData)))
-    end
-    for phaseBoundType in [(1, 2), (2, 3)]
-        currentTransitionWindow = copy(transitionWindow)
-        currentPoleFractions = [PoleFraction(size_BZ, omega_by_t, kondoJ, W_val; availableData=availableData, loadData=loadData) for W_val in currentTransitionWindow]
-        currentPhaseIndices = map(fracToIndex, currentPoleFractions)
-        numIter = 1
-        while abs(currentTransitionWindow[1] - currentTransitionWindow[2]) > bathIntSpacing && numIter < maxIter
-            updatedEdge = 0.5 * sum(currentTransitionWindow)
-            newPoleFraction = PoleFraction(size_BZ, omega_by_t, kondoJ, updatedEdge; availableData=availableData, loadData=loadData)
-            newPhaseIndex = fracToIndex(newPoleFraction)
-            if newPhaseIndex == currentPhaseIndices[1] || newPhaseIndex == phaseBoundType[1]
-                currentPhaseIndices[1] = newPhaseIndex
-                currentTransitionWindow[1] = updatedEdge
-            else
-                currentPhaseIndices[2] = newPhaseIndex
-                currentTransitionWindow[2] = updatedEdge
-            end
-            numIter += 1
-        end
-        push!(criticalBathInt, 0.5 * sum(currentTransitionWindow))
-    end
-    criticalBathIntData[string(bathIntSpacing)] = criticalBathInt
-
-    open(savePath, "w") do file JSON3.write(file, availableData) end
-    open(savePathCrit, "w") do file JSON3.write(file, criticalBathIntData) end
-
-    return criticalBathInt
-end
-
+# Given a range of values for J and W, counts the 
+# number of Kondo relevant Fermi surface points 
+# for every value and plots it as a colourmap on J X W
 function PhaseDiagram(
         size_BZ::Int64,
         kondoJVals, 
         bathIntVals,
     )
-    polesFractionMatrix = zeros(length(bathIntVals), length(kondoJVals))
-    @showprogress for (x, J_val) in enumerate(kondoJVals)
-        for (y, W_val) in enumerate(bathIntVals)
-            polesFractionMatrix[y, x] = PoleFraction(size_BZ, J_val, W_val)
-        end
+    # matrix in the space of J_i x W_j. Stores the number of
+    # gapless points on Fermi surface (see `CountPoles` for
+    # details) at any given parameter point (J_i, W_j)
+    polesMatrix = zeros(length(bathIntVals), length(kondoJVals))
+
+    # This can be parallelised for large gains. 
+    # Uncomment the line below and comment out 
+    # the line below it to enable multithreading.
+    # Julia must be started with somthing like 
+    # `julia -t 5` to leverage this, where 5 is 
+    # the number of threads.
+    
+    # @showprogress Threads.@threads for ((x, J_val), (y, W_val)) in Iterators.product(enumerate(kondoJVals), enumerate(bathIntVals)) |> collect
+    @showprogress for ((x, J_val), (y, W_val)) in Iterators.product(enumerate(kondoJVals), enumerate(bathIntVals))
+        polesMatrix[y, x] = CountPoles(size_BZ, J_val, W_val)
     end
-    p = heatmap(polesFractionMatrix, title=size_BZ)
+    p = heatmap(polesMatrix, title="\$N = $size_BZ\$", xlabel="\$J\$", ylabel="\$W\$")
     display(p)
 end
 
-PhaseDiagram(33, 0.05:0.01:0.3, 0:-0.01:-0.4)
+PhaseDiagram(33, 0.05:0.04:0.3, 0:-0.05:-0.4)
